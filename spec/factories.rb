eaf3768abb551d4e3b009b97b5622fe295f548dd
RundongGao <asphinx423@gmail.com>
2015-11-29 16:36:21 -0500

    integrate with rspec

---
+++ spec/factories.rb
@@ -0,0 +1,18 @@
+
+FactoryGirl.define do
+  factory :climber do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
+  end
+
+  trait :public do
+    public true
+  end
+
+  trait :private do
+    public false
+  end
+end
+
