d7e62146702cc25b20453218666dd6679f0cc3c2
RundongGao <asphinx423@gmail.com>
2015-12-12 17:06:40 -0500

    rspec for training_index

--- spec/controller/trainings_controller_spec.rb
+++ spec/controller/trainings_controller_spec.rb
@@ -20,13 +20,54 @@ RSpec.describe TrainingsController, type: :controller do
 
 
 		context 'is records owner' do
-			it 'assgine @is_owner with true'
-			it 'render index template'
+			before do
+				private_climber_id = FactoryGirl.create(:climber, :private)
+				sign_in private_climber_id		
+				get :index, climber_id: private_climber_id
+			end
+
+			it 'assgine @is_owner with true' do
+				assigns(:is_owner).should be true
+			end
+			it 'render index template' do
+				expect(response).to render_template(:index) 
+			end
 		end
 
-		context 'is not records owner' do 
-			it 'assgine @is_owner with false'
-			it 'render index template'
+		context 'is not records owner of a public record' do 
+			before do
+				sign_in_climber_id = FactoryGirl.create(:climber, :private)		
+				other_public_climber_id = FactoryGirl.create(:climber, :public)	
+
+				sign_in sign_in_climber_id	
+				get :index, climber_id: other_public_climber_id
+			end
+
+			it 'assgine @is_owner with false' do
+				assigns(:is_owner).should be false
+			end
+
+			it 'render index template' do
+				expect(response).to render_template(:index) 
+			end
+		end
+
+		context 'is not records owner of a private record' do 
+			before do
+				sign_in_climber_id = FactoryGirl.create(:climber, :private)		
+				other_private_climber_id = FactoryGirl.create(:climber, :private)	
+
+				sign_in sign_in_climber_id	
+				get :index, climber_id: other_private_climber_id
+			end
+
+			it 'assgine @is_owner with false' do
+				assigns(:is_owner).should be false
+			end
+			
+			it 'redirect back to root page' do
+				expect(response).to redirect_to(:root)
+			end
 		end
 	end
 end
